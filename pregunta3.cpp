1a3e1f3a82ed74add9b03bc93b3fd621907171e0
Lostwin <smile_184@hotmail.com>
2024-09-02 14:28:03 -0500

    creamos la funcion de binarySearch para encontrar las tablas indexadas

---
+++ pregunta3.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+int binarySearch(vector<int> &arr,int n,int key){
+	int l=0;
+	int r=n-1;
+	int mid;
+
+	while(l<=r){
+		mid=(l+r)/2;
+		if(arr[mid]==key)
+			return mid;
+		else if(key>arr[mid])
+			l=mid+1;
+		else 
+			r=mid-1;
+	}
+	return -1;
+}
+
+
+
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t=1;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
