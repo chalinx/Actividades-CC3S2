6a714badf06f4e1668cd64bda18cc3053c585a35
Lostwin <smile_184@hotmail.com>
2024-09-02 13:48:45 -0500

    creamos la funcion dijkstra, creamos un vector dis que representa la distancia entre los nodos inicialmente, tienen una distancia infinitam, y tambien creamos una cola de prioridad para obtener las distancias minima al nodo vecino

---
+++ pregunta1.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i]; 
+
+using namespace std;
+
+class Graph {
+	int V;
+
+	list<pair<int,int>>*l;
+public:
+	Graph(int v){
+		this->V=v;
+		l=new list<pait<int,int>>[V];
+	}
+
+    void addEdge(int u, int v, int wt, bool undir = true) {
+        l[u].push_back({wt, v});
+
+        if (undir)
+            l[v].push_back({wt, u});
+    }
+
+
+s
+};
+
+
+void solve(){
+	
+		
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("D:/Problem Set Competitiva/input.txt","r",stdin);
+freopen("D:/Problem Set Competitiva/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
