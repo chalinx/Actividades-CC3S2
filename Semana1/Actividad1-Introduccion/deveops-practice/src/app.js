bf90d352dc323bf9f723ca815013b9a241c7d370
chalinx <smile_184@hotmail.com>
2024-09-09 01:39:41 -0500

    borrado archivo sublime

--- Semana1/Actividad1-Introduccion/deveops-practice/src/app.js
+++ Semana1/Actividad1-Introduccion/deveops-practice/src/app.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-module.exports = app;
-
-
-if (require.main === module) {
-    const port = process.env.PORT || 3000; 
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
-}
+ const app = express();
+ app.get('/', (req, res) => {
+ res.send('Hello, World!');
+ });
+ const port = process.env.PORT || 3000;
+ app.listen(port, () => {
+console.log(`Server running on port ${port}`);
+ });
+ module.exports = app;
