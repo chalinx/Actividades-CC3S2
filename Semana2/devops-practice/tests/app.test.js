b4fffab52be386f93afcf7e1e129bd78e7876be6
chalinx <smile_184@hotmail.com>
2024-09-13 18:49:37 -0500

    corregir Package.json

--- Semana2/devops-practice/tests/app.test.js
+++ Semana2/devops-practice/tests/app.test.js
@@ -1,9 +1,15 @@
 const request = require('supertest');
- const app = require('../src/app');
- describe('GET /', () => {
- it('should return Hello, World!', async () => {
- const res = await request(app).get('/');
- expect(res.statusCode).toEqual(200);
- expect(res.text).toBe('Hello, World!');
- });
- });
+const app = require('../src/app');
+
+describe('GET /', () => {
+  it('should return Hello, World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, World!');
+  });
+});
+
+afterAll(done => {
+  // Cierra cualquier operación asíncrona o servidor que pueda estar activo
+  done();
+});
